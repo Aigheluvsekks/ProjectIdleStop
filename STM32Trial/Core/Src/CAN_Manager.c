2a47186448eca3dfe0b79f0fb30fdfe3e9c34ab3
bonderite-sniffer <gadingsakti.work@gmail.com>
2026-08-15 16:32:59 +0900

    Update CAN_Manager.c

--- STM32Trial/Core/Src/CAN_Manager.c
+++ STM32Trial/Core/Src/CAN_Manager.c
@@ -6,16 +6,17 @@
   */
 
 #include "CAN_Manager.h"
+#include "CAN_Decode.h"
 
-void CAN_Manager_Init(CAN_HandleTypeDef *hcan) {
+void CAN_Manager_Init(CAN_HandleTypeDef *hcan1) {
     /* 1. Start the CAN peripheral */
-    HAL_CAN_Start(hcan);
+    HAL_CAN_Start(hcan1);
 
     /* 2. Activate interrupts for both FIFOs so we don't miss messages */
-    HAL_CAN_ActivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_RX_FIFO1_MSG_PENDING);
+    HAL_CAN_ActivateNotification(hcan1, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_RX_FIFO1_MSG_PENDING);
 }
 
-void CAN_Manager_ConfigFilter_StdID(CAN_HandleTypeDef *hcan, uint32_t filterBank, uint32_t targetID, uint32_t mask) {
+void CAN_Manager_ConfigFilter_StdID(CAN_HandleTypeDef *hcan1, uint32_t filterBank, uint32_t targetID, uint32_t mask) {
     CAN_FilterTypeDef filterConfig = {0};
 
     /*
@@ -39,14 +40,14 @@ void CAN_Manager_ConfigFilter_StdID(CAN_HandleTypeDef *hcan, uint32_t filterBank
     filterConfig.SlaveStartFilterBank = 14;                    /* Relevant only if using dual CAN (CAN1/CAN2) */
 
     /* Apply the filter to the hardware */
-    HAL_CAN_ConfigFilter(hcan, &filterConfig);
+    HAL_CAN_ConfigFilter(hcan1, &filterConfig);
 }
 
-bool CAN_Manager_ReadMessage(CAN_HandleTypeDef *hcan, uint32_t rxFifo, CAN_Msg_t *pMsg) {
+bool CAN_Manager_ReadMessage(CAN_HandleTypeDef *hcan1, uint32_t rxFifo, CAN_Msg_t *pMsg) {
     CAN_RxHeaderTypeDef rxHeader;
 
     /* Attempt to pull the message out of the hardware FIFO */
-    if (HAL_CAN_GetRxMessage(hcan, rxFifo, &rxHeader, pMsg->Data) == HAL_OK) {
+    if (HAL_CAN_GetRxMessage(hcan1, rxFifo, &rxHeader, pMsg->Data) == HAL_OK) {
 
         /* Check if it's a Standard or Extended ID frame */
         pMsg->isExtID = (rxHeader.IDE == CAN_ID_EXT);
@@ -60,16 +61,23 @@ bool CAN_Manager_ReadMessage(CAN_HandleTypeDef *hcan, uint32_t rxFifo, CAN_Msg_t
     return false; /* FIFO was empty or error */
 }
 
-void CAN_Manager_RxCallback(CAN_HandleTypeDef *hcan) {
+void CAN_Manager_RxCallback(CAN_HandleTypeDef *hcan1) 
+{
     CAN_Msg_t incomingMsg;
 
     /* Read from FIFO 0 */
     if (CAN_Manager_ReadMessage(hcan, CAN_RX_FIFO0, &incomingMsg)) {
 
         /* Example data handling logic: */
-        if (incomingMsg.ID == 0x123) {
+        if (incomingMsg.ID == 0x11F) {
+        	CAN_DecodeMessage(incomingMsg.ID, incomingMsg.Data, incomingMsg.DLC);
             // Do something with incomingMsg.Data[0] ...
         }
 
     }
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    CAN_Manager_RxCallback(hcan);
 }
