334b7833b129d993677b4911e37332d7d69870d1
bonderite-sniffer <gadingsakti.work@gmail.com>
2026-08-15 09:54:38 +0900

    Update CAN_Decode.h
    Added multipliers and offsets for RPM value

--- STM32Trial/Core/Inc/CAN_Decode.h
+++ STM32Trial/Core/Inc/CAN_Decode.h
@@ -3,42 +3,52 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "Validity.h"
+
+//Offset for conversion to actual data
+#define RPM_FACTOR 0.12350f
+#define RPM_OFFSET -39.5f
 
 typedef struct
 {
+	uint16t raw_rpm
 	uint16_t rpm;
-	bool rpm_Valid;
+	dataValidity_t rpm_Valid;
 
+/*
+ *
+ * DISCLAIMER: NEED TO ADD RAW_(VARIABLE NAME FIRST) FOR DECODING PURPOSES
 	uint16_t swingR_Press;
 	uint16_t swingL_Press;
-	bool swingL_Press_Valid;
-	bool swingR_Press_Valid;
+	dataValidity_t swingL_Press_Valid;
+	dataValidity_t swingR_Press_Valid;
 
 	uint16_t BoomD_Press;
 	uint16_t BoomUP_Press;
-	bool BoomD_Press_Valid;
-	bool BoomUP_Press_Valid;
+	dataValidity_t BoomD_Press_Valid;
+	dataValidity_t BoomUP_Press_Valid;
 
 	uint16_t ArmDig_Press;
-	bool ArmDig_Press_Valid;
+	dataValidity_t ArmDig_Press_Valid;
 
 	uint16_t BucketDump_Press;
-	bool BucketDump_Press_Valid;
+	dataValidity_t BucketDump_Press_Valid;
 
 	uint16_t TravelLR_Press;
 	uint16_t TravelLF_Press;
 	uint16_t TravelRR_Press;
 	uint16_t TravelRF_Press;
-	bool TravelLR_Press_Valid;
-	bool TravelLF_Press_Valid;
-	bool TravelRR_Press_Valid;
-	bool TravelRF_Press_Valid;
+	dataValidity_t TravelLR_Press_Valid;
+	dataValidity_t TravelLF_Press_Valid;
+	dataValidity_t TravelRR_Press_Valid;
+	dataValidity_t TravelRF_Press_Valid;
 
 	// uint16_t RPump_Press;
 	// uint16_t LPump_Press;
 	// uint16_t Engine_Temp;
-}
-MachineData_t;
+	 *
+	 */
+} MachineData_t;
 
 void CAN_DecodeMessage(
 uint32_t id,
