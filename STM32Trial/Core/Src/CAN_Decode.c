a3cc7f81aac749b73a473eaa7f8cdc33ec434eb6
bonderite-sniffer <gadingsakti.work@gmail.com>
2026-08-15 09:50:23 +0900

    Updated CAN_Decode.c
    Added the function to automatically convert CAN Big Endian Little Endian value into decimals

--- STM32Trial/Core/Src/CAN_Decode.c
+++ STM32Trial/Core/Src/CAN_Decode.c
@@ -1,44 +1,59 @@
 #include "CAN_Decode.h"
 
-/* 1. STRUCT INITIALIZATION FIXES */
 static MachineData_t machine_data =
 {
-    .rpm = 0,                 // Use a dot (.) and comma (,) instead of '=' and ';'
-    .swingR_Press = 0,
-    .swingL_Press = 0,
-    .BoomD_Press = 0,
-    .BoomUP_Press = 0,
-    .ArmDig_Press = 0,
-    .BucketDump_Press = 0,
-    .TravelLR_Press = 0,
-    .TravelLF_Press = 0,
-    .TravelRR_Press = 0,
-    .TravelRF_Press = 0       // No comma needed on the last active item
-    //.RPump_Press  = 0,
-    //.LPump_Press = 0,
-    //.Engine_Temp = 0
-};                            // Add the missing semicolon here
+		.rpm = 0
+
+		// erase the /* */ to connect or decode other var. or systems
+	/*.swingR_Press = 0,
+		.swingL_Press = 0,
+		.BoomD_Press = 0,
+		.BoomUP_Press = 0,
+		.ArmDig_Press = 0,
+		.BucketDump_Press = 0,
+		.TravelLR_Press = 0,
+		.TravelLF_Press = 0,
+		.TravelRR_Press = 0,
+		.TravelRF_Press = 0       // No comma needed on the last active item
+	  	.RPump_Press  = 0,
+      	.LPump_Press = 0,
+      	.Engine_Temp = 0
+       * */
+};
 
 void CAN_DecodeMessage(
-    uint32_t id,
-    uint8_t *data,
-    uint8_t dlc
+	uinr32_t id,
+	uint8_t *data,
+	uint8_t dlc
 )
 {
-    /* 2. SWITCH STATEMENT FIXES */
-    switch(id)
-    {                         // Add missing opening brace for the switch body
-        case 0x100:           // Add missing colon (:) after the case value
-            if (dlc >= 2)
-            {
-                /* Assuming Big-Endian format (MSB in data[0], LSB in data[1]) */
-                machine_data.rpm = ((uint16_t)data[0] << 8) | data[1];
-            }
-            break;
+	switch(id)
+	{
+	case 0x11f //RPM Case
+	if (dlc >= 2) //Data Byte size
+	{
+		raw_rpm=
+				((uint16_t)data[0] << 8) | //Unfinished
+				data[1];
 
-        case 0x101:           // Add missing colon (:)
-            // Add parsing logic for SwingR_Press here
-            break;            // Always remember the break statement
+		machine_data.rpm =
+				(raw_rpm*RPM_FACTOR) + RPM_OFFSET;
+	}
 
-    }                         // Add missing closing brace for the switch body
+	break;
+
+ /*
+  * Currently only rpm data is read and used since the main
+  * hydraulic pump is controlled by / connected to the engine with shaft
+  * power derived from the main engine
+  * for additional addition please contact at gadingsakti.work@gmail.com or
+  * make new function / if loop based on big endian and little endian conversion
+  * big endian and little endian need to be looked for based on trial and error
+  * and the matlab code provided on the github link below
+  *
+  * https://github.com/Aigheluvsekks/ProjectIdleStop/tree/main
+  *
+  */
+
+}
 }
