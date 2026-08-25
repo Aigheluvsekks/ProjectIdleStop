7ad16e4b22a410961f66e78ab85765d8f4efb8ee
bonderite-sniffer <gadingsakti.work@gmail.com>
2026-08-26 07:53:40 +0900

    Update Timer_Manager.c

--- STM32Trial/Core/Src/Timer_Manager.c
+++ STM32Trial/Core/Src/Timer_Manager.c
@@ -16,16 +16,33 @@ static uint32_t targetCountdownTimeMs = 0;
 static uint32_t startTimeTick = 0;
 static bool     isTimerRunning = false;
 
+static OperatingTime_t activeOperatingTime = TIME_INTERVAL_1;
+
 
 /* Debug */
 volatile uint32_t debug_timer_remaining_ms = 0;
 volatile uint32_t debug_timer_remaining_s = 0;
 
 /* --- Function Implementations --- */
+void Timer_Manager_SetOperatingTime(OperatingTime_t opTime)
+{
+    if (opTime == TIME_INTERVAL_1 ||
+        opTime == TIME_INTERVAL_2 ||
+        opTime == TIME_INTERVAL_3)
+    {
+        activeOperatingTime = opTime;
+    }
+}
+
+OperatingTime_t Timer_Manager_GetOperatingTime(void)
+{
+    return activeOperatingTime;
+}
+
 
 void Timer_Manager_Start(void) {
     /* 1. Mengetahui waktu countdown yang harus dieksekusi */
-    OperatingTime_t opTime = IO_GetOperatingTime();
+	OperatingTime_t opTime = activeOperatingTime;
 
     /* 2. Menentukan target millisec berdasarkan interval time1, time2, time3 */
     switch ((uint8_t)opTime) {
@@ -103,3 +120,5 @@ bool Timer_Manager_IsExpired(void) {
 
     return false;
 }
+
+
